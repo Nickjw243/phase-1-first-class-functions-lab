42c1bdbeab2bdb1f58c326e5d4639193a8d7828f
Nick Weiland <njweiland23@gmail.com>
2023-10-31 18:02:06 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2)
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function fareMultiplier(fare) {
+        return fare * multiplier
+    }
+}
+function fareDoubler(fare) {
+    return createFareMultiplier(2) (fare)
+}
+function fareTripler(fare) {
+    return createFareMultiplier(3) (fare)
+}
+let selectDifferentDrivers = (drivers, fn) => {
+    return fn(drivers)
+}
